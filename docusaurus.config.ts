62e2b3ed2e3cb80521389aab9549553ec17b4551
Danial <sdnoaein@gmail.com>
2023-11-10 14:53:32 +0330

    config base url for vercel

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://danialnoaein.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: `/nextjs-course/`,
+  baseUrl: `/`,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
